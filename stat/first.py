edcba3229a5fa5ea38c92d0e507662c9de4d0848
amol <amoljb007@gmail.com>
2024-12-06 23:49:31 +0530

    pyhton

--- stat/first.py
+++ stat/first.py
@@ -1,2 +1,96 @@
 # first code
 # mean median 
+
+
+
+'''
+
+
+### **Conceptual Questions**  
+1. What is a probability distribution? Can you differentiate between discrete and continuous probability distributions?  
+it is outcome rv expt along with its probabliyic values;
+
+dicreste  are distinct values of random variable such as coin toss
+continius in values are in range 
+
+
+2. Explain the differences between a probability density function (PDF) and a cumulative distribution function (CDF).  
+
+pdf is function that describe likelihood of a continus random variable within specific range
+cdf is sum of pdf
+
+
+3. What is the difference between a uniform distribution and a normal distribution? Provide use cases for each.  
+
+uniform distrubution are having value same for each x pint, ex dice rolling .
+while there are diffrent values of normal distrubution bell shaped peak at mean centred at mean
+ex adult height
+
+
+
+4. Define and differentiate between a Binomial distribution and a Poisson distribution. When would you use one over the other?  
+
+
+
+
+
+5. Explain the concept of the Central Limit Theorem and its significance in data science. 
+
+clt it is important therom in maths and stat it basically teel us that if we have sample size ore than 30 then 
+of any distrubution need not be gausian then mean of \
+    random sampled datapoint will be following normal distrubution
+
+
+### **Practical Application Questions**  
+6. How would you verify if a dataset follows a normal distribution? Name some statistical tests and visualization techniques.  
+
+
+QQ plot 
+
+
+7. Suppose you have a dataset with skewed data. What probability distribution might model it better, and how would you transform it into a normal distribution?  
+
+log normal , box cox transformation
+
+8. How can probability distributions be used to model uncertainty in machine learning predictions?  
+
+### **Real-world Problem Solving**  
+9. In a call center, you observe that the number of calls received per minute is highly variable.
+ Which probability distribution would you use to model the number of calls, and why?  
+
+
+
+
+10. If an e-commerce site experiences 10 purchases per hour on average, what is the probability of observing exactly 15 purchases in an hour? Which distribution would you use to calculate this?  
+
+### **Coding/Implementation**  
+11. Write Python code to generate 1000 samples from a normal distribution with a mean of 50 and a standard deviation of 10. Plot its histogram.  
+
+
+
+
+12. Given a dataset of customer waiting times, write Python code to fit an exponential distribution and estimate its parameters.  
+
+
+
+
+
+13. How would you simulate data from a binomial distribution in Python for an experiment with 20 trials and a success probability of 0.7?  
+
+### **Advanced Questions**  
+14. Explain the concept of a joint probability distribution and how it differs from marginal and conditional distributions.  
+
+
+
+
+15. Discuss the concept of entropy in probability distributions. How is it relevant to data science?  
+
+
+
+
+16. What is a multivariate normal distribution, and how is it applied in machine learning models?  
+
+
+
+
+'''
