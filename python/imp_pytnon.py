59d36228c982c668eded653db89c71c2a14b34dd
amol <amoljb007@gmail.com>
2025-01-13 22:52:11 +0530

    py

---
+++ python/imp_pytnon.py
@@ -0,0 +1,4 @@
+# Palindrome Check: Determines if a string is a palindrome by comparing characters.
+# Second Largest Element: Finds the second largest element in an array using two approaches.
+# Divisors: Identifies all numbers that divide a given number evenly.
+# Equal Sum Subarrays: Locates a subarray with a sum equal to a previous prefix sum
