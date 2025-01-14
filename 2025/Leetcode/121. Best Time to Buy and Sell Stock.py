13d1036af4b04c1b62140db3f717c4cb3509058b
amol <amoljb007@gmail.com>
2025-01-14 23:32:24 +0530

    py

--- 2025/Leetcode/121. Best Time to Buy and Sell Stock.py
+++ 2025/Leetcode/121. Best Time to Buy and Sell Stock.py
@@ -1,1 +1,23 @@
-# 
+# psudocode
+
+
+a=[1,3,5,7,2]
+
+
+# find lowest value while going right
+# find highest value while going right
+# and take diffrence
+
+lowest=float('inf')
+max_profit=0
+
+for price in a:
+    if price<lowest:
+        lowest=price
+
+        profit=price-lowest
+
+    if profit>max_profit:
+        max_profit=profit
+
+return max_profit
