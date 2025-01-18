9d5bc76eace7d5cf524d8f6c0fae159079c05664
amoljb007 <amoljb007@gmail.com>
2025-01-18 09:03:09 +0000

    py

---
+++ 2025/Leetcode/jump.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        last_pos=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]+i >=last_pos:
+                last_pos=i
+        return last_pos==0
+        
