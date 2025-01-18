e6ddb263b1330ab7f19e37b00e8aafe9a9e82d22
amoljb007 <amoljb007@gmail.com>
2025-01-18 09:01:56 +0000

    py

---
+++ 2025/Leetcode/pluspne.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits):
+        strings = ""
+        for number in digits:
+            strings += str(number)
+
+        temp = str(int(strings) +1)
+
+        return [int(temp[i]) for i in range(len(temp))]
+
--- idx/first.py
+++ idx/first.py
@@ -1,1 +1,13 @@
 # first code on idx machine
+
+import pandas as pd
+
+
+# Creating a raw DataFrame
+data = {
+    'order_id': [101, 102, 103, 104, 105],
+    'order_date': ['2023-05-01', '2023-06-15', '2023-07-20', '2023-08-25', '2023-09-30'],
+    'delivery_date': ['2023-05-05', '2023-06-20', '2023-07-25', '2023-08-30', '2023-10-05'],
+    'amount': [250, 400, 150, 300, 500]
+}
+df = pd.DataFrame(data)
